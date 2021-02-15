0d8524d30a805a56e7fc440776d1cda94b86badc
Franck Gallard <franckgnaika@gmail.com>
2021-02-15 18:55:24 -0500

    ma directive bas de page

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,25 +1,35 @@
 <template>
   <div>
-    <footer>
-      Footer
-    </footer>
+    <footer v-footerDirective></footer>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Footer'
-}
+  name: "Footer",
+  directives: {
+    footerDirective: {
+      inserted: function(el) {
+        el.innerHTML =
+          "Copyright © " +
+          new Date().getUTCFullYear() +
+          "- Franck Gallard - All rights reserved.";
+      },
+    },
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-    footer {
-        margin: 5px;
-        bottom: 0;
-        position: relative;
-        justify-content: center;
-        text-align: center;
-        width: 100%;
-    }
+footer {
+  margin-top: 50px;
+  bottom: 0;
+  position: relative;
+  justify-content: center;
+  text-align: center;
+  width: 100%;
+  background-color: black;
+  color: white;
+}
 </style>
